302d1e3326decd6da079f326f76b874f75267952
Im-Crs <christopheramador88@gmail.com>
2021-02-05 15:15:00 -0500

    finished

--- week4attempt2/components/Sidebar.js
+++ week4attempt2/components/Sidebar.js
@@ -7,12 +7,18 @@ class Sidebar extends Component {
   }
 
   render () {
-   
+
     let responseHTML = '';
+
 
     this.props.menu.forEach( (item) => {
       // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
+      responseHTML += '<li>' + item + '</li>';
+      // ^ this wraps each item in the responseHTML into an <li> element.
     });
+
+responseHTML = '<ul>' + responseHTML + '</ul>';
+//This wraps the entire responseHTML into an <ul> element
 
     return responseHTML;
   }
